2fce90cb9832870add45be69723677d873035694
Nzioka6350 <jamesnziokamutua1@gmail.com>
2023-08-10 01:50:32 -0700

    Code without betty error

---
+++ betty.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - check which number is the greatest
+ *
+ * Return: Always returs 0.
+ */
+
+int main(void)
+{
+	int j, k;
+
+	printf("Enter j:\n");
+	scanf("%d ", &j);
+	printf("Enter k:\n");
+	scanf("%d ", &k);
+
+	if (j > k)
+	{
+		printf("J is the largest\n");
+	}
+	else
+	{
+		printf("K is the largest\n");
+	}
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,9 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#endif 
