082ed29901366b17e1c00d4e40c08cd03e14ac62
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-08-17 05:02:00 -0700

    env 5

--- shell.h
+++ shell.h
@@ -8,6 +8,7 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <stddef.h>
 
 #define MAX_TOKENS 100
 #define PATH_MAX_LENGTH 1024
@@ -16,5 +17,6 @@
 void execution(char **argv);
 char *get_location(char *command);
 int exiting(int ac, char **argv);
+void print_env(void);
 
 #endif 
