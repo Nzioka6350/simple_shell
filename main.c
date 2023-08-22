1b31c58110ded702bd655c4e6f5264cfae0e7f72
Nzioka6350 <jamesnziokamutua1@gmail.com>
2023-08-22 05:22:04 -0700

    Main.c done

--- main.c
+++ main.c
@@ -1,64 +1,70 @@
 #include "shell.h"
+
 /**
- * main - check the code
+ * main - Check the code
  *
  * Return: Always 0.
  */
-int main()
+int main(void)
 {
-	char incmd[100];
-	char *insert;
-	char inpcmd[MAX_COMMAND_LENGTH];
-	char args[MAX_COMMAND_LENGTH];
-	 char *args[3];
-    char *token;
-    int i;
+		char incmd[100];
+		char *insert;
+		char inpcmd[MAX_COMMAND_LENGTH];
+		char args[MAX_COMMAND_LENGTH];
 
-	while(1)
-	{
-		can_display();
-		handle_inputs(incmd, sizeof(incmd));
-		if(strcmp(incmd, "exit") ==0)
+		while (1)
 		{
-			buldin_exiting();
+			can_display();
+			handle_inputs(incmd, sizeof(incmd));
+
+			if (strcmp(incmd, "exit") == 0)
+			{
+				buldin_exiting();
+			}
+			else if (strcmp(incmd, "env") == 0)
+			{
+				_environ();
+			}
+			else
+			{
+				execution(incmd);
+				arguments_exec(incmd);
+			}
+
+			shell_print("Enter anything:");
+			insert = ourOwn_shellgetline();
+
+			tokenize_cmd(insert, inpcmd, args);
+			printf("First Word or Command: %s\n", inpcmd);
+			printf("Arguments: %s\n", args);
+
+			free(insert);
+
+			if (strncmp(inpcmd, "setenv ", 7) == 0)
+			{
+				char *token = strtok(args, " ");
+				if (token != NULL)
+				{
+					setenv(token, args + strlen(token) + 1, 1);
+				}
+				else
+				{
+					fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
+				}
+			}
+			else if (strncmp(inpcmd, "unsetenv ", 9) == 0)
+			{
+				char *variable = args + 9;
+				if (*variable != '\0')
+				{
+					unsetenv(variable);
+				}
+				else
+				{
+					fprintf(stderr, "Usage: unsetenv VARIABLE\n");
+				}
+			}
 		}
-		else if (strcmp(incmd, "env") == 0)
-		{
-			_environ();
-                }else if (strncmp(incmd, "setenv ", 7) == 0) {
-            token = strtok(incmd + 7, " ");
-            args[0] = token;
 
-            i = 1;
-            while (token != NULL && i < 3) {
-                token = strtok(NULL, " ");
-                args[i] = token;
-                i++;
-            }
-	      if (args[0] != NULL && args[1] != NULL) {
-                setenv_command(args[0], args[1]);
-            } else {
-                fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
-            }
-        } else if (strncmp(incmd, "unsetenv ", 9) == 0) {
-            char *variable = incmd + 9;
-            if (variable != NULL) {
-                unsetenv_command(variable);
-            } else {
-                fprintf(stderr, "Usage: unsetenv VARIABLE\n");
-            }
-        } else {
-			execution(incmd);
-			arguments_exec(incmd);
-		}
-		shell_print("Enter anything:");
-		insert = ourOwn_shellgetline();
-
-		tokenize_cmd(insert, inpcmd, args);
-		printf("First Word or Command: %s\n", inpcmd);
-		printf("Arguments : %s\n", args);
-
-		free(insert);
-        }
-	return (0);
+		return (0);
 }
