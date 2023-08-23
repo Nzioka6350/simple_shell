fb6ba5f5bdd32a6d645933a344d3ec276a2571df
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-08-23 02:53:42 -0700

    arguments

---
+++ arguments.c
@@ -0,0 +1,42 @@
+#include "shell.h"
+
+/**
+ * arguments_exec - Function that handles the arguments.
+ * @incmd: Command typed.
+ *
+ * Return: Always 0.
+ */
+
+void arguments_exec(const char *incmd)
+{
+	pid_t toto_pid = fork();
+
+	if (toto_pid == -1)
+	{
+		perror("Fork error");
+		exit(EXIT_FAILURE);
+	}
+	else if (toto_pid == 0)
+	{
+		char *args[100];
+		int no_arguments = 0;
+
+		char *token = strtok((char *)incmd, " ");
+
+		while (token != NULL)
+		{
+			args[no_arguments++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[no_arguments] = NULL;
+
+		execvp(args[0], args);
+
+		shell_print("Error occurred\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
