484038c080471b4c72db0cdf9c9b4a6995da6b64
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-08-23 02:57:51 -0700

    shell.h

--- shell.h
+++ shell.h
@@ -14,19 +14,18 @@
 #define MAX_TOKENS 100
 #define PATH_MAX_LENGTH 1024
 #define INPUT_LENGTH 100
-/*Buffer sizes are here*/
+/* Buffer sizes are here */
 #define BUFF_SIZE 700
 #define MAX_COMMAND_LENGTH 100
 
-/*End of the size declaration make sure you place all of then in-between*/
+/* Function prototypes */
 void execution(const char *incmd);
 char *get_location(char *command);
 int exiting(int ac, char **argv);
 int prompting(int ac, char **argv);
 int _larger(void);
 void print_env(void);
-
-/*My second prototypes*/
+/* My second prototypes */
 void can_display(void);
 void handle_inputs(char *incmd, size_t size);
 void shell_print(const char *Inputs);
@@ -38,5 +37,6 @@ void setenv_command(char *variable, char *value);
 void unsetenv_command(char *variable);
 char *ourOwn_shellgetline(void);
 void tokenize_cmd(char *cmdin, char *incmd, char *args);
-/*End of the prototypes*/
+/* End of the prototypes */
+
 #endif
