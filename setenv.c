c3e03f3417e1cda1cb0e109b05c933c1f5ae8939
Nzioka6350 <jamesnziokamutua1@gmail.com>
2023-08-23 04:43:04 -0700

    changes

--- setenv.c
+++ setenv.c
@@ -5,23 +5,6 @@
  *
  *
  */
-void tokenize_cmd(char *cmdin, char *incmd, char *args)
-{
-        char *space = strchr(cmdin, ' ');
-        if (space != NULL)
-        {
-                strncpy(incmd, cmdin, space - cmdin);
-                incmd[space - cmdin] = '\0';
-                strcpy(args, space + 1);
-        } else
-        {
-                strcpy(incmd, cmdin);
-                args[0] = '\0';
-        }
-}
-root@0ba2c5c15596:/simple_shell# cat setenv.c
-#include "shell.h"
-
 void unsetenv_command(char *variable) {
     if (unsetenv(variable) != 0) {
         fprintf(stderr, "Failed to unset environment variable.\n");
