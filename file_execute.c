132999a911f96b9b13894469f71f0044f4b1364f
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-08-24 10:31:39 -0700

    file execute

--- file_execute.c
+++ file_execute.c
@@ -1,8 +1,17 @@
 #include "shell.h"
+
 /**
  * read_file - Read Command From File
  * @filename:Filename
  * @argv:Program Name
+ * This function reads commands from
+ * a file specified by `filename`. It reads each
+ * line from the file, parses the command,
+ * and executes it if necessary. The function
+ * terminates when all lines in the file have been processed.
+ *
+ * @filename: The name of the file to read commands from.
+ * @argv: The program name
  * Return: -1 or  0
  */
 
@@ -29,12 +38,23 @@ void read_file(char *filename, char **argv)
 	fclose(fp);
 	exit(0);
 }
+
 /**
  * treat_file - PARSE Check Command Fork Wait Excute in Line of File
  * @line: Line From A File
  * @counter:Error Counter
  * @fp:File Descriptor
  * @argv:Program Name
+ * This function takes a line from a file,
+ * parses the command, and executes it if necessary.
+ * It handles built-in commands, external commands,
+ * and also the "exit" command specific
+ * to processing a file.
+ *
+ * @line: The line read from the file.
+ * @counter: The error counter (for error reporting).
+ * @fp: The file descriptor of the opened file.
+ * @argv: The program name.
  * Return : Excute A line void
  */
 void treat_file(char *line, int counter, FILE *fp, char **argv)
@@ -59,11 +79,24 @@ void treat_file(char *line, int counter, FILE *fp, char **argv)
 			free(cmd);
 		}
 }
+
 /**
  * exit_bul_for_file - Exit Shell Case Of File
  * @line: Line From A File
  * @cmd: Parsed Command
  * @fd:File Descriptor
+ * This function handles the "exit" command when
+ * processing a line from a file. If no
+ * argument is provided, it exits the program
+ * with the current error number. If an
+ * argument is provided, it attempts to convert
+ * it to an integer and exits with that
+ * status code. If the argument is not a valid integer,
+ * an error message is printed.
+ *
+ * @cmd: The parsed command.
+ * @line: The line from the file.
+ * @fd: The file descriptor of the opened file.
  * Return : Case Of Exit in A File Line
  */
 void exit_bul_for_file(char **cmd, char *line, FILE *fd)
