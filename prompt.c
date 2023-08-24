a61806fb04106f3d6e854abd3d479fa1cd66fd9b
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-08-24 11:33:04 -0700

    prompt

--- prompt.c
+++ prompt.c
@@ -2,16 +2,25 @@
 
 /**
  * prompt - Display Shell Prompt
+ * This function is responsible for displaying the
+ * shell prompt, typically represented
+ * as a "$" symbol, indicating that the shell
+ * is ready to accept user input.
  */
 void prompt(void)
 {
 	PRINTER("$ ");
 }
+
 /**
  * print_error - Display Error Based on Command and How Many Time Shell Looped
  * @input:User Input
  * @counter:Simple Shell Count Loop
  * @argv:Program Name
+ * This function displays an error message
+ * when a command is not found.
+ * it includes information about the program name,
+ * loop counter, and the command that was not found.
  * Return: Void
  */
 void print_error(char *input, int counter, char **argv)
