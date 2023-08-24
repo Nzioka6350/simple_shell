6c8e662d371729c719aa9c8d672de6c1ca90b28c
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-08-24 11:27:12 -0700

    print

--- print.c
+++ print.c
@@ -2,6 +2,10 @@
 /**
  * print_number -Print Unsigned Int Putchar
  * @n: Unisigned Integer
+ * This function prints an unsigned integer using
+ * the _putchar function.
+ * It recursively breaks down the integer into its
+ * individual digits and prints them.
  * Return: Void
  */
 void print_number(unsigned int n)
@@ -16,6 +20,11 @@ void print_number(unsigned int n)
 /**
  * print_number_in -Print Number Putchar
  * @n:Integer
+ * This function prints an integer using the _putchar function.
+ * If the integer is negative, it prints a minus sign
+ * and then handles the absolute value.
+ * It recursively breaks down the absolute value into
+ * its individual digits and prints them.
  * Return: void
  */
 void print_number_in(int n)
