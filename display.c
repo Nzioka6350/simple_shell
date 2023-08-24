3553b4a3eea7da50897e1c6b544d0f2a5fe624c2
sarahsyombua2002 <sarahsyombua2002@gmail.com>
2023-08-24 10:13:22 -0700

    display

--- display.c
+++ display.c
@@ -1,9 +1,18 @@
 #include "shell.h"
+
 /**
  *  _prerror - Print Custome Error
  * @argv:Program Name
  * @c:Error Count
  * @cmd:Command
+ * This function is responsible for printing a custom
+ * error message to the standard output.
+ * It prints an error message indicating that
+ * an illegal number was provided as an argument.
+ *  @argv: The name of the program that encountered the error.
+ * @c: The error count or index, typically used to track
+ * the number of commands executed.
+ * @cmd: The command that caused the error, along with its arguments
  * Return: Void
  */
 void _prerror(char **argv, int c, char **cmd)
